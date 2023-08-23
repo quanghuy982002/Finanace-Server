000d88e0bc6d0b6f1e76d46018321c1bc85f051c
minh do <minhquangdo201@gmail.com>
2023-08-23 20:03:03 +0700

    add role & staff data

--- application/src/main/java/org/example/controller/RoleController.java
+++ application/src/main/java/org/example/controller/RoleController.java
@@ -1,13 +1,17 @@
 package org.example.controller;
 
+import org.example.dto.PageResponse;
+import org.example.dto.Response;
+import org.example.entity.Role;
 import org.example.request.role.RoleRequest;
+import org.example.request.role.RoleSearchRequest;
 import org.example.service.impl.RoleServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/role")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class RoleController {
     private final RoleServiceImpl roleService;
 
@@ -23,5 +27,30 @@ public class RoleController {
     @PostMapping("/create")
     public ResponseEntity<?> createRole(@RequestBody RoleRequest request){
         return this.roleService.createRole(request);
+    }
+
+    @GetMapping("/get-staffs/{id}")
+    public ResponseEntity<?> getStaffs(@PathVariable Integer id){
+        return this.roleService.getStaffsByRoleId(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getRoleById(@PathVariable Integer id){
+        return this.roleService.getRoleById(id);
+    }
+
+    @PostMapping("/search")
+    public PageResponse<Role> searchRole(@RequestBody RoleSearchRequest request){
+        return roleService.searchRole(request);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateRole(@PathVariable Integer id, @RequestBody RoleRequest request){
+        return this.roleService.updateRole(id,request);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> deleteRole(@PathVariable Integer id){
+        return this.roleService.deleteRole(id);
     }
 }
--- application/src/main/java/org/example/controller/StaffController.java
+++ application/src/main/java/org/example/controller/StaffController.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/staff")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class StaffController {
     private final StaffServiceImpl service;
 
